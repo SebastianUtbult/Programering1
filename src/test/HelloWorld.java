ff00326950f2f7f02500dec8b2a7a63166f15031
Sebastian Utbult <sebbeprooo@gmail.com>
2018-09-05 16:24:19 +0200

    Project - HelloWorld C3

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -9,18 +9,18 @@ public class HelloWorld {
 		Scanner input = new Scanner(System.in);
 		
 		int x = input.nextInt();
-		int y = input.nextInt();
+		int yuuuh = input.nextInt();
 		
-			if(x>0 && y>0) {
+			if(x>0 && yuuuh>0) {
 				System.out.println(1);
 			}
-			if(x>0 && y<0) {
+			if(x>0 && yuuuh<0) {
 				System.out.println(4);
 			}
-			if(x<0 && y>0) {
+			if(x<0 && yuuuh>0) {
 				System.out.println(3);
 			}
-			if(x<0 && y<0) {
+			if(x<0 && yuuuh<0) {
 				System.out.println(2);
 			}
 			
