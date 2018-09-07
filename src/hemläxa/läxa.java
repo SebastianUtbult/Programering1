f87b1760d38f651cef7c40bb98cb86ae11f85c9f
Sebastian Utbult <sebbeprooo@gmail.com>
2018-09-07 15:05:26 +0200

    Hemläxa Commit2

--- src/hemläxa/läxa.java
+++ src/hemläxa/läxa.java
@@ -1,7 +1,18 @@
 package hemläxa;
 
+import java.util.Scanner;
+
 public class läxa {
 	public static void main(String[] args) {
+		
+		Scanner input = new Scanner (System.in);
+		
+		System.out.println("Vad är ditt namn?");
+		
+		String namn = input.nextLine();
+		
+		System.out.print("Du heter ");
+		System.out.print(namn);
 		
 	}
 }
