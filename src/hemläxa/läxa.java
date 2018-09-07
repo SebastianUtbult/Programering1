fa0e36aed51e8b473f3b3c36f3235c9983784d35
Sebastian Utbult <sebbeprooo@gmail.com>
2018-09-07 14:56:00 +0200

    HemLäxa C1

---
+++ src/hemläxa/läxa.java
@@ -0,0 +1,7 @@
+package hemläxa;
+
+public class läxa {
+	public static void main(String[] args) {
+		
+	}
+}
--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -9,19 +9,19 @@ public class HelloWorld {
 		Scanner input = new Scanner(System.in);
 		
 		int x = input.nextInt();
-		int yuuuh = input.nextInt();
+		int y = input.nextInt();
 		
-			if(x>0 && yuuuh>0) {
-				System.out.println(2);
+			if(x>0 && y>0) {
+				System.out.println("kvadrant 2");
 			}
-			if(x>0 && yuuuh<0) {
-				System.out.println(4);
+			if(x>0 && y<0) {
+				System.out.println("kvadrant 4");
 			}
-			if(x<0 && yuuuh>0) {
-				System.out.println(1);
+			if(x<0 && y>0) {
+				System.out.println("kvadrant 1");
 			}
-			if(x<0 && yuuuh<0) {
-				System.out.println(3);
+			if(x<0 && y<0) {
+				System.out.println("kvadrant 3");
 			}
 			
 	}	//main	
