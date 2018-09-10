6b4d6c6d7c93ac118c28f8476a9b958b8d8dd56f
Sebastian Utbult <sebbeprooo@gmail.com>
2018-09-10 17:20:56 +0200

    Hemläxa färdig

--- src/hemläxa/läxa.java
+++ src/hemläxa/läxa.java
@@ -5,15 +5,26 @@ import java.util.Scanner;
 public class läxa {
 	public static void main(String[] args) {
 		
+		String M = ", ";
+		
 		Scanner input = new Scanner (System.in);
 		
-		System.out.println("Vad är ditt namn?");
-		
+		System.out.print("Vad är ditt namn?");
 		String namn = input.nextLine();
+		
+		System.out.print("Vad är din adress?");
 		String adress = input.nextLine();
+		
+		System.out.print("Vad är ditt postnummer?");
 		String Postnummer = input.nextLine();
+		
+		System.out.print("Vad är din stad?");
 		String stad = input.nextLine();
+		
+		System.out.print("Vad är ditt telefonnummer?");
 		String telefonnummer = input.nextLine();
 		
+		System.out.println("Upgifter:");
+		System.out.println(namn +M+ adress +M+ Postnummer +M+ telefonnummer);
 	}
 }
