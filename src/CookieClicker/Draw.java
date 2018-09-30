211b4da54a39e078f85ddc249bb8cc8a50691160
Sebastian Utbult <sebbeprooo@gmail.com>
2018-09-30 12:53:01 +0200

    Cookie Clicker C3
    COOKIES
    COOKIES PER SEC

--- src/CookieClicker/Draw.java
+++ src/CookieClicker/Draw.java
@@ -1,15 +1,25 @@
 package CookieClicker;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.text.DecimalFormat;
 
 import javax.swing.JLabel;
 
 public class Draw extends JLabel{
+	
 	IL i = new IL();
 	Gui gui = new Gui();
+	
+	DecimalFormat df = new DecimalFormat("###, ###, ###");
+	DecimalFormat df2 = new DecimalFormat("###, ###, ###");
+	
+	String s;
+	int textwidth;
+	
 	protected void paintComponent(Graphics g){
 		
 		super.paintComponent(g);
@@ -22,11 +32,22 @@ public class Draw extends JLabel{
 		
 		//övre lista
 		g.setColor(Color.black);
-		g.fillRect(0, 0, gui.width, 100);
+		g.fillRect(0, 0, gui.width, 100); 
 		
+		//mitten linje
 		g.setColor(Color.WHITE);
 		g.drawLine(gui.width/2, 100, gui.width/2, gui.height);
 		g.drawLine(0, 100, gui.width, 100);
+		
+		//lite text
+		g.setFont(new Font("Arial", Font.BOLD, 30));
+		s = df.format(Main.cookies) + " C";
+		textwidth = g.getFontMetrics().stringWidth(s);
+		g.drawString(s, gui.width/4 - textwidth/2, 62);
+		
+		g.setFont(new Font("Arial", Font.BOLD, 15));
+		s = df2.format(Main.cps) + " C/s";
+		g.drawString(s, 10, 88);
 		
 		repaint();
 		
--- src/CookieClicker/Main.java
+++ src/CookieClicker/Main.java
@@ -2,6 +2,12 @@ package CookieClicker;
 
 public class Main {
 
+	static double cookies = 0;
+	static double cps = 10;
+	static double cpc = 1;
+	static int ugAnzahl = 2;
+	
+	
 	public static void main(String[] args) {
 		
 		Gui g = new Gui();
